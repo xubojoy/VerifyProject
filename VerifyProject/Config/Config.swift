cdc208546268da60194e125a3a37a476b3c0c4eb
xubojoy <xubojoy1112@163.com>
2017-08-24 15:38:50 +0800

    组件化

---
+++ VerifyProject/Config/Config.swift
@@ -0,0 +1,10 @@
+//
+//  Config.swift
+//  VerifyProject
+//
+//  Created by xubojoy on 2017/8/24.
+//  Copyright © 2017年 xubojoy. All rights reserved.
+//
+
+import Foundation
+
---
+++ VerifyProject/Pods/Target Support Files/Pods-VerifyProject/Pods-VerifyProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VerifyProject : NSObject
+@end
+@implementation PodsDummy_Pods_VerifyProject
+@end
